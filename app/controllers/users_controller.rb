0b006f1b8d207a609867d1b1373b27983e5b219b
Azumi S <49363119+sazumy@users.noreply.github.com>
2020-12-23 00:08:12 +0900

    userが新規作成できるようにする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,13 @@
 #
 #  index_users_on_email  (email) UNIQUE
 #
-class User < ApplicationRecord
+class User < ActiveRecord::Base
   authenticates_with_sorcery!
+
+  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+
+  validates :email, uniqueness: true
+  validates :username, presence: true
 end
