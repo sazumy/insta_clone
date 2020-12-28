9aa87c1ea15f93fec58309b48e8e9d94eb1f0e68
Azumi S <49363119+sazumy@users.noreply.github.com>
2020-12-28 23:34:43 +0900

    fakerのインストール、seedデータの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+  user = User.create!(
+    username: Faker::JapaneseMedia::StudioGhibli.character,
+    email: Faker::Internet.email,
+    password: 'foobar',
+    password_confirmation: 'foobar',
+  )
+  puts "\"#{user.username}\" has created!"
+end
