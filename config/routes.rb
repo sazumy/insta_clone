deb91906a77707783dbb9ebad1b98621efa5ab1d
Azumi S <49363119+sazumy@users.noreply.github.com>
2020-12-29 17:57:57 +0900

    とりあえずposts#indexがみられるようにする

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
-  root 'users#new'
+  root 'posts#index'
 
   resources :users, only: %i(new create)
+
+  resources :posts
 end
