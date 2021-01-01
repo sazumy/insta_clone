820c8e9fb57380f79531a9c39175b7d3563c2665
Azumi S <49363119+sazumy@users.noreply.github.com>
2021-01-01 21:19:35 +0900

    投稿の削除機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -40,6 +40,10 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy!
+    redirect_to posts_path
+    flash[:success] = '投稿を削除しました'
   end
 
   private
