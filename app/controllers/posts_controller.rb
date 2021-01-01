417a19d234684fc0066fd02a4fcf81b1f1fc9e83
Azumi S <49363119+sazumy@users.noreply.github.com>
2021-01-01 21:12:07 +0900

    投稿の編集機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,7 +29,14 @@ class PostsController < ApplicationController
   end
 
   def update
-    @updated_post = PostForm.new(post_params, post: @post)
+    @post = PostForm.new(post_edit_params.merge(user_id: current_user.id))
+    if @post.save!
+      redirect_to root_path
+      flash[:success] = "投稿を編集しました"
+    else
+      flash.now[:danger] = "投稿の編集に失敗しました"
+      render :edit
+    end
   end
 
   def destroy
@@ -37,8 +44,13 @@ class PostsController < ApplicationController
 
   private
 
+  # TODO: post_paramsとpost_edit_paramsを統合する
   def post_params
     params.require(:post_form).permit(:body, photoes: [])
+  end
+
+  def post_edit_params
+    params.require(:post).permit(:body, photoes: [])
   end
 
   def set_post
