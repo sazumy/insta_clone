49862d1fc0781cef0dcf1358f2863df48a535d37
Azumi S <49363119+sazumy@users.noreply.github.com>
2021-01-07 21:31:27 +0900

    ページネーションを動作させる

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
 
   def index
     @users = User.all
-    @posts = Post.includes(:user, :images).order(created_at: 'DESC')
+    @posts = Post.includes(:user, :images).order(created_at: 'DESC').page(params[:page]).per(3)
   end
 
   def new
