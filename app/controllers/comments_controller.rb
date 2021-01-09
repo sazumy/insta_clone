00132d1bb17864391a216c1ff773ba0d40c5e3c9
Azumi S <49363119+sazumy@users.noreply.github.com>
2021-01-09 21:21:22 +0900

    若干のリファクタリング

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: %i[destroy edit update]
+
   def create
     @comment = current_user.comments.build(comment_params)
     @comment.save!
