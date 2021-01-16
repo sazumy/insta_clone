5f1449ee104f3bfdec1ed29df43ca567dee0207c
Azumi S <49363119+sazumy@users.noreply.github.com>
2021-01-16 23:13:47 +0900

    通信をAjaxにする

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,11 +2,12 @@ class RelationshipsController < ApplicationController
   before_action :require_login, only: %i[create destroy]
 
   def create
-    @follower = User.find(params[:follower])
-    current_user.follow(follower: @follower)
+    @other_user = User.find(params[:follower])
+    current_user.follow(@other_user)
   end
 
   def destroy
+    @user = current_user.relationships.find(params[:id]).follower
     current_user.unfollow(params[:id])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -33,7 +33,7 @@ class User < ApplicationRecord
   has_many :relationships, dependent: :destroy
   has_many :followings, through: :relationships, source: :follower
 
-  has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'follower_id'
+  has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
   has_many :followers, through: :passive_relationships, source: :user
 
   def own?(object)
@@ -52,9 +52,9 @@ class User < ApplicationRecord
     like_posts.destroy(post)
   end
 
-  def follow(user)
-    unless self == user[:follower]
-      relationships.find_or_create_by!(follower: user[:follower])
+  def follow(other_user)
+    unless self == other_user
+      relationships.find_or_create_by!(follower: other_user)
     end
   end
 
