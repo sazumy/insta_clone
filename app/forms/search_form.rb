a8b9685fd3d5b61d672817f15d006b7d586649eb
Azumi S <49363119+sazumy@users.noreply.github.com>
2021-01-31 16:32:30 +0900

    ユーザー名検索メソッドの微修正

--- app/forms/search_form.rb
+++ app/forms/search_form.rb
@@ -15,7 +15,7 @@ class SearchForm
       @posts = @posts.or(Post.includes(:user, :images).where('body LIKE ?', "%#{keyword}%")) if body_keywords.present?
     end
 
-    @posts = Post.joins(:users).includes(:user, :images).where('username LIKE ?', "%#{user_name}%") if user_name.present?
+    @posts = Post.joins(:user).includes(:user, :images).where('username LIKE ?', "%#{user_name}%") if user_name.present?
     @posts = Post.joins(:comments).includes(:user, :images).where('comments.body LIKE ?', "%#{comment_body}%") if comment_body.present?
 
     return Kaminari.paginate_array(@posts)
