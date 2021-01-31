60573aea7bea1db383a6f8a114080bd7b4a27fca
Azumi S <49363119+sazumy@users.noreply.github.com>
2021-01-31 14:50:27 +0900

    コメントでも検索可能にする

--- app/forms/search_form.rb
+++ app/forms/search_form.rb
@@ -12,11 +12,12 @@ class SearchForm
     @posts = Post.none
 
     body_keywords.each do |keyword|
-      @posts = @posts.or(Post.includes(:user, :images).where('body LIKE ?', "%#{keyword}%")) if user_name.present?
+      @posts = @posts.or(Post.includes(:user, :images).where('body LIKE ?', "%#{keyword}%")) if body_keywords.present?
     end
 
-    @posts += Post.joins(:user).where('username LIKE ?', "%#{user_name}%") if user_name.present?
-    @posts += Post.joins(:comment).where('body LIKE ?', "%#{comment_body}%") if comment_body.present?
+    @posts = Post.joins(:users).includes(:user, :images).where('username LIKE ?', "%#{user_name}%") if user_name.present?
+    @posts = Post.joins(:comments).includes(:user, :images).where('comments.body LIKE ?', "%#{comment_body}%") if comment_body.present?
+
     return Kaminari.paginate_array(@posts)
   end
 
