684431c3727a0e88783049a1f6605f15732f0310
Azumi S <49363119+sazumy@users.noreply.github.com>
2021-02-13 23:08:15 +0900

    アバターの表示

--- app/controllers/mypage/accounts_controller.rb
+++ app/controllers/mypage/accounts_controller.rb
@@ -1,9 +1,27 @@
 class Mypage::AccountsController < ApplicationController
   before_action :require_login
+  before_action :set_user
 
   def edit
   end
 
   def update
+    if @user.update(account_params)
+      redirect_to edit_mypage_account_path, success: 'プロフィールを更新しました'
+    else
+      flash.now['danger'] = 'プロフィールの更新に失敗しました'
+      render :edit
+    end
   end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def account_params
+    params.require(:user).permit(:email, :username, :avatar, :avatar_cache)
+  end
+
 end
