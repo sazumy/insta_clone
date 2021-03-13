677955507ec63baa883ded85f7e082c9defd6851
Azumi S <49363119+sazumy@users.noreply.github.com>
2021-03-13 15:42:22 +0900

    Revert "action_mailerとletter_openerの設定"
    This reverts commit defed54c14a500a36b3c1574363f0c8b3ddcb68b.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-
-  if Rails.env.development?
-    mount LetterOpenerWeb::Engine, at: "/letter_opener"
-  end
-
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
@@ -30,5 +25,4 @@ Rails.application.routes.draw do
     resource :account, only: %i[edit update]
     resources :activities, only: %i[index]
   end
-
 end
